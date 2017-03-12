be0ac77bdf91ac27e396f2e803c319330d77b736
icancode23 <ernipunarora@gmail.com>
2017-03-12 15:07:50 +0530

    added the Api.py

---
+++ Api.py
@@ -0,0 +1,29 @@
+############################# API FOR HuffPost Chatbot ##############################
+def mapLatitudeToPincode(lattitude,longitude):
+	################# give constituency info based on latitude and longitude #######
+	pass
+def getConstituency(pincode=None,latitude=None,longitude=None):
+	########## get constituency name from  pincode given it is not null else call constituency mapping function and save it in the current user's database ########
+	pass
+
+def registerUserLocation(fbid,pincode=None,latitude=None,longitude=None):
+	########### register the user location by getConstituency #######
+	pass
+
+def constituencyInfo(fbid):
+	################ Gives Manifesto link,MlA name according to location in Fbid############
+	if (constituency==Noida)
+	if (constituency==Mainpuri)
+	if (constituency==Karhal)
+	pass
+def registerIssue(fbid,Issue):
+	######### Register Issue at your OWN location by getting name from calling getConstituency funtion to get  and Trigger on database to check whether the issue has been repoted many times#############
+	pass
+def getMLADetails(fbid):
+	######## FROM  fbid queryuser location and give mla suitably ##########
+	
+	pass
+
+def genElectionSummary(electionName):
+	####################  get the election name at select the csv you want to query for ad return the tota #########
+	pass
